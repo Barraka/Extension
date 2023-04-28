065fe12b5eadca1e07c6c5dbe008ac49d6e0fb21
Barraka <girault.e@gmail.com>
2023-04-28 16:22:51 +0200

    Added fonts

--- popup.js
+++ popup.js
@@ -1,5 +1,54 @@
 const play = document.querySelector('#play');
 const darkMode = document.querySelector('#darkMode');
+const fontChooser = document.querySelector('.fontChooser');
+const fontWrapper = document.querySelector('.fontWrapper');
+if(fontWrapper)fontWrapper.addEventListener('click', e=> {
+    handleFont(e);
+});
+
+if(fontChooser)fontChooser.addEventListener('click', async ()=> {
+    if(!fontChooser.classList.contains('active')) {
+        fontChooser.classList.add('active');
+        fontWrapper.classList.remove('hidden');
+    } else {
+        
+        fontChooser.classList.remove('active');
+        fontWrapper.classList.add('hidden');
+    }
+});
+
+async function handleFont(e) {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    //----Apply css with fonts
+    try {
+        const response = await fetch(chrome.runtime.getURL("font.css"));
+        const css = await response.text();
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            const tab = tabs[0];
+            chrome.scripting.insertCSS({
+                target: { tabId: tab.id },
+                css: css,
+            });
+        });
+    } catch(err) { 
+        console.log('Error: ', err);
+    }
+
+    //----Change tag style
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: applyfont,
+        args: [e.target.id]
+    });  
+    window.close();
+}
+function applyfont(font) {
+    const allTags=document.body.querySelectorAll('*');
+    allTags.forEach(tag=> {
+           tag.style.fontFamily=`${font}`;
+    });
+    
+}
 
 if(darkMode)darkMode.addEventListener('click', async ()=> {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -7,14 +56,17 @@ if(darkMode)darkMode.addEventListener('click', async ()=> {
         target: { tabId: tab.id },
         function: runDarkMode,
     });
+    window.close();
 });
 
 function runDarkMode() {
     const allTags=document.body.querySelectorAll('*');
     allTags.forEach(tag=> {
            tag.style.backgroundColor="rgb(32,32,32)";
-           tag.style.color="aliceBlue";
+           if(tag.tagName==='A')tag.style.color="green";
+           else tag.style.color="aliceBlue";
     });
+    document.body.style.backgroundColor="rgb(32,32,32)";
 }
 
 if(play)play.addEventListener('click', async ()=> {
@@ -39,6 +91,7 @@ if(play)play.addEventListener('click', async ()=> {
     .catch((error) => {
     console.error("Error loading CSS file:", error);
     });
+    window.close();
 });
 
 async function runlink(l) {
